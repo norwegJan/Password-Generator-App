3bdfbc5980d2436c0ac5ad58be20e1d315d39989
norwegJan <236618355+norwegJan@users.noreply.github.com>
2026-04-06 14:20:33 +0200

    continue with validation- and generator-logic

--- script.js
+++ script.js
@@ -3,10 +3,21 @@
 const copyBtn = document.querySelector("#copy-btn");
 const copiedMsg = document.querySelector("#copied");
 
-const sliderInput = document.querySelector("#char-slider");
-const numberLength = document.querySelector("#number-length");
+const sliderInput = document.querySelector("#slider-input");
+const numberEl = document.querySelector("#number-el");
 
-let numberValue = 0;
+const uppercaseCheck = document.querySelector("#uppercase-check");
+const lowercaseCheck = document.querySelector("#lowercase-check");
+const numbersCheck = document.querySelector("#numbers-check");
+const symbolsCheck = document.querySelector("#symbols-check");
+
+const generateBtn = document.querySelector("#generate-btn");
+const errorMsg = document.querySelector("#error-msg");
+
+const uppercasePool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercasePool = "abcdefghijklmnopqrstuvwxyz";
+const numbersPool = "0123456789";
+const symbolsPool = "$*.-[];:?@+%!{},=()#&/_~";
 
 // ADD EVENT LISTENERS
 
@@ -16,17 +27,60 @@ copyBtn.addEventListener("click", (e) => {
 
 sliderInput.addEventListener("input", renderSliderState);
 
+generateBtn.addEventListener("click", (e) => {
+  const canGenerate = validateInputRenderError();
+
+  if (canGenerate) {
+    generatePassword();
+  }
+});
+
 // DECLARE FUNCTIONS
 
 function renderSliderState() {
-  numberValue = sliderInput.valueAsNumber;
-
+  const lengthValue = sliderInput.valueAsNumber;
   const min = sliderInput.min;
   const max = sliderInput.max;
-  const progress = ((numberValue - min) / (max - min)) * 100;
+  const progress = ((lengthValue - min) / (max - min)) * 100;
 
-  numberLength.textContent = numberValue;
+  numberEl.textContent = lengthValue;
   sliderInput.style.setProperty("--progress", `${progress}%`);
 }
+
+function getInclusionPools() {
+  let inclusionPools = [];
+
+  if (uppercaseCheck.checked) {
+    inclusionPools.push(uppercasePool);
+  }
+  if (lowercaseCheck.checked) {
+    inclusionPools.push(lowercasePool);
+  }
+  if (numbersCheck.checked) {
+    inclusionPools.push(numbersPool);
+  }
+  if (symbolsCheck.checked) {
+    inclusionPools.push(symbolsPool);
+  }
+  return inclusionPools;
+}
+
+function validateInputRenderError() {
+  const lengthValue = sliderInput.valueAsNumber;
+  const inclusionPools = getInclusionPools();
+  let generateGreenLight = false;
+
+  if (inclusionPools.length === 0 || lengthValue < inclusionPools.length) {
+    generateBtn.classList.add("error-state");
+    errorMsg.textContent = "Temporary placeholder error text";
+  } else {
+    generateBtn.classList.remove("error-state");
+    errorMsg.textContent = "";
+    generateGreenLight = true;
+  }
+  return generateGreenLight;
+}
+
+function generatePassword() {}
 
 renderSliderState();
